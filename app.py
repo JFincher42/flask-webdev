0438f7c1bc3ade9d5ae6814377579309c90af087
Jon Fincher <jfincher42@gmail.com>
2022-06-15 14:07:43 -0500

    Adding Bootstrap, some templates

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, redirect, url_for, session, flash
 from flask_bootstrap import Bootstrap
 
@@ -56,3 +57,24 @@ def songs():
 @app.route("/derived")
 def derived():
     return render_template("derived.html")
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to Emily's Dog Costumes!"
+
+
+@app.route('/services')
+def services():
+    return "I offer custom made costumes for your precious canine companion, "\
+        "and a free in-home consultation, to get the measurements."
+
+
+@app.route('/costumes/<costume>')
+def costumes(costume):
+    return f"Check out this {costume} costume!"
+>>>>>>> e770a7e (Adding Bootstrap, some templates)
