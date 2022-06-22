03a87dea37ba8b8c3b7e0567d5f890eedd707f87
Jon Fincher <jfincher42@gmail.com>
2022-06-22 12:39:47 -0500

    Unit 7 stuff

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, flash
 from flask_bootstrap import Bootstrap
+
+# For the form
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = "good for debug, sucks for prod"
 
 @app.route("/")
 def index():
     return "Hello Web World!"
+
+@app.route("/form", methods=["GET", "POST"])
+def show_form():
+    # name = None
+    # form = NameForm()
+    # if form.validate_on_submit():
+    #     name = form.name.data
+    #     form.name.data = ''
+    #     return redirect(url_for("show_form"))
+    # return render_template('index.html', form=form, name=name)
+
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        flash("Great! Hope you enjoy your stay!")
+        return redirect(url_for('show_form'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route("/about")
