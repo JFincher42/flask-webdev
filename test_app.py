fcf0d4c3e2d98c8d94fa974ba1f6c320306494b9
Jon Fincher <jfincher42@gmail.com>
2022-06-30 09:35:26 -0500

    Adding some test stuff

---
+++ test_app.py
@@ -0,0 +1,33 @@
+from app import create_app, db
+from app.models import User
+from flask import current_app
+
+
+def test_app_creation():
+    app = create_app("testing")
+    assert app
+
+
+def test_current_app():
+    app = create_app("testing")
+    app.app_context().push()
+    assert current_app
+    assert current_app.config["TESTING"]
+
+
+def test_database_insert():
+    app = create_app("testing")
+    assert app.config["TESTING"]
+    # ensure test database configured
+    assert "mysql+pymysql" in app.config["SQLALCHEMY_DATABASE_URI"]
+    app.app_context().push()
+    # create all tables if not created
+    db.create_all()
+
+    u = User(username="john")
+    db.session.add(u)
+    db.session.commit()
+
+    # IMPORTANT clear database for future tests
+    db.session.remove()
+    db.drop_all()
