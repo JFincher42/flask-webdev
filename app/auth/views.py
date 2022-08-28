0bb5837b174ecd8979ee8b474e5e482a738fa12d
Jon Fincher <jfincher42@gmail.com>
2022-08-27 20:28:00 -0500

    Email confirmation code halfway done

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 # Get the stuff we need from flask
 from flask import render_template, redirect, url_for, flash, request, current_app
-from flask_login import logout_user, login_user
+from flask_login import logout_user, login_user, login_required
 from .forms import LoginForm, RegistrationForm
 from ..models import User
 from .. import db, email
@@ -51,11 +51,23 @@ def register():
         db.session.add(user)
         db.session.commit()
 
+        # Generate confirmation token and send link
+        token = user.generate_confirmation_token()
+        email.send_mail(
+            email_entered, "Confirm your email", "auth/email/confirm", token=token
+        )
+
         # Welcome the user
-        email.send_mail(email_entered, "Registered!", "mail/welcome", user=user)
+        # email.send_mail(email_entered, "Registered!", "mail/welcome", user=user)
 
         # Notify the admin
-        email.send_mail(current_app.config["APP_ADMIN"], "New User", "mail/new_user", user=user, time=datetime.now())
+        email.send_mail(
+            current_app.config["APP_ADMIN"],
+            "New User",
+            "mail/new_user",
+            user=user,
+            time=datetime.now(),
+        )
 
         flash(f"User {username_entered} registered, please login!")
         return redirect(url_for(".login"))
@@ -68,3 +80,41 @@ def logout():
     logout_user()
     flash("You've been logged out successfully!")
     return redirect(url_for("main.index"))
+
+
+@auth.route("/confirm/<token>")
+@login_required
+def confirm(token):
+    # If they're already confirmed, this is not needed
+    if current_user.confirmed:
+        flash("You're already confirmed, silly!")
+        return redirect(url_for("main.index"))
+
+    # Otherwise, we check the confirmation token, and confirm as necessary
+    if current_user.confirm(token):
+        db.session.commit()
+        flash("You have confirmed your account! Thank you.")
+    else:
+        flash("Whoops! That confirmation link either expired, or it isn't valid.")
+
+    # In either case, head back to the main page
+    return redirect(url_for("main.index"))
+
+
+@auth.before_app_request
+def before_request():
+    if (
+        current_user.is_authenticated
+        and not current_user.confirmed
+        and request != "static"
+        and request.blueprint != "auth"
+        and request.endpoint != "static"
+    ):
+        return redirect(url_for("auth.unconfirmed"))
+
+
+@auth.route("/unconfirmed")
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for("main.index"))
+    return render_template("auth/unconfirmed.html", user=current_user)
